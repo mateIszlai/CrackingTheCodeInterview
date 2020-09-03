47dbd221d66093ea17d27dda588ae7050f9589c1
Máté Iszlai <iszimate@gmail.com>
2020-09-03 12:24:27 +0200

    Test return not null

--- Technical Questions/TechnicalQuestionsTests/PermutationLocatorTests.cs
+++ Technical Questions/TechnicalQuestionsTests/PermutationLocatorTests.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using TechnicalQuestions;
 
 namespace TechnicalQuestionsTests
 {
+    [TestFixture]
     public class PermutationLocatorTests
     {
+        private readonly PermutationLocator _locator = new PermutationLocator();
+
+        [Test]
+        public void PermutationNumberReturn()
+        {
+            Assert.NotNull(_locator.GetPermutationsNumber("a", "a"));
+        }
     }
 }
