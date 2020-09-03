3b068f0483c3c2fe6280d0cf733284fc82e02420
Máté Iszlai <iszimate@gmail.com>
2020-09-03 14:04:51 +0200

    Add tests to more words

--- Technical Questions/TechnicalQuestionsTests/RansomNoteTests.cs
+++ Technical Questions/TechnicalQuestionsTests/RansomNoteTests.cs
@@ -34,5 +34,61 @@ namespace TechnicalQuestionsTests
             _note = new RansomNote("a");
             Assert.IsFalse(_note.isFormedFrom("b"));
         }
+
+        [Test]
+        public void IsFormedFrom_GivenMoreCharacter_ReturnsTrue()
+        {
+            _note = new RansomNote("abc");
+            Assert.IsTrue(_note.isFormedFrom("Mbczwzcwcb a hfuehwf abc"));
+        }
+
+        [Test]
+        public void IsFormedFrom_GivenMoreCharacter1_ReturnsTrue()
+        {
+            _note = new RansomNote("a abc abcd");
+            Assert.IsTrue(_note.isFormedFrom("zgzq abc wgx ab a abhjfe  abcd"));
+        }
+
+        [Test]
+        public void IsFormedFrom_GivenMoreCharacter2_ReturnsTrue()
+        {
+            _note = new RansomNote("aacbebwc viewv ejd9ewh");
+            Assert.IsTrue(_note.isFormedFrom(" viewv fhzeuwf wefgz aacbebwc ahfew jvehwuiv ejd9ewh"));
+        }
+
+        [Test]
+        public void IsFormedFrom_GivenMoreCharacter3_ReturnsTrue()
+        {
+            _note = new RansomNote("aa aa bbcd bb bb");
+            Assert.IsTrue(_note.isFormedFrom(" aa bb fhzeuwf bb aacbebwc aa jvehwuiv bbcd ejd9ewh."));
+        }
+
+        [Test]
+        public void IsFormedFrom_GivenMoreCharacter_ReturnsFalse()
+        {
+            _note = new RansomNote("ab");
+            Assert.IsFalse(_note.isFormedFrom("b"));
+        }
+
+        [Test]
+        public void IsFormedFrom_GivenMoreCharacter1_ReturnsFalse()
+        {
+            _note = new RansomNote("ab");
+            Assert.IsFalse(_note.isFormedFrom("ba vubvzuwbv iucweiu a vrive"));
+        }
+
+        [Test]
+        public void IsFormedFrom_GivenMoreCharacter2_ReturnsFalse()
+        {
+            _note = new RansomNote("abcd jewhw dde");
+            Assert.IsFalse(_note.isFormedFrom("dde czwcw abcd cjwc"));
+        }
+
+        [Test]
+        public void IsFormedFrom_GivenMoreCharacter3_ReturnsFalse()
+        {
+            _note = new RansomNote("abcd jewhw abcd dde");
+            Assert.IsFalse(_note.isFormedFrom("dde czwcw ewhw abcd cjwc"));
+        }
     }
 }
