cc10cda9e5bdc68b200d5d430faff93bdd49f99c
Máté Iszlai <iszimate@gmail.com>
2020-09-03 12:36:10 +0200

    Create tests to small is one character

--- Technical Questions/TechnicalQuestionsTests/PermutationLocatorTests.cs
+++ Technical Questions/TechnicalQuestionsTests/PermutationLocatorTests.cs
@@ -13,5 +13,30 @@ namespace TechnicalQuestionsTests
         {
             Assert.NotNull(_locator.GetPermutationsNumber("a", "a"));
         }
+
+        [Test]
+        public void GetPermutationsNumber_Return_1()
+        {
+            var small = "a";
+            var big = "a";
+
+            Assert.AreEqual(1, _locator.GetPermutationsNumber(small, big));
+        }
+
+        [Test]
+        public void GetPermutationsNumber_GivenLongString_Return_1()
+        {
+            var small = "a";
+            var big = "abcdledddddjwurn";
+            Assert.AreEqual(1, _locator.GetPermutationsNumber(small, big));
+        }
+
+        [Test]
+        public void GetPermutationsNumber_GivenLongString_Return_5()
+        {
+            var small = "a";
+            var big = "abcdaleddaddadjawurn";
+            Assert.AreEqual(5, _locator.GetPermutationsNumber(small, big));
+        }
     }
 }
