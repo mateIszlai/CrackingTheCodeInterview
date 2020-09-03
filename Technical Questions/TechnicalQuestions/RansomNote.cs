5d4f428f8e5647b40734d08641b44ff5a731b7be
Máté Iszlai <iszimate@gmail.com>
2020-09-03 13:34:00 +0200

    Create classes to second excercise

---
+++ Technical Questions/TechnicalQuestions/RansomNote.cs
@@ -0,0 +1,22 @@
+﻿namespace TechnicalQuestions
+{
+    public class RansomNote
+    {
+        /*
+         * Example: A ransom note can be formed by cutting words out of a magazine to form a new
+         * sentence.How would you figure out if a ransom note(represented as a string) can be formed
+         * from a given magazine(string)?
+         */
+
+        private readonly string _note;
+        public RansomNote(string note)
+        {
+            _note = note;
+        }
+
+        public bool isFormedFrom(string magazine)
+        {
+            return false;
+        }
+    }
+}
---
+++ Technical Questions/TechnicalQuestionsTests/RansomNoteTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using TechnicalQuestions;
+
+namespace TechnicalQuestionsTests
+{
+    [TestFixture]
+    public class RansomNoteTests
+    {
+        RansomNote _note;
+
+        [TearDown]
+        public void TearDown()
+        {
+            _note = null;
+        }
+
+        [Test]
+        public void IsFormedFrom_Returns()
+        {
+            _note = new RansomNote("a");
+            Assert.IsNotNull(_note.isFormedFrom("b"));
+        }
+    }
+}
