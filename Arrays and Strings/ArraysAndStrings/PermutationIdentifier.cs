02f2df8b97a9b84b90528b3394fa01a0e56b9deb
Máté Iszlai <iszimate@gmail.com>
2020-09-04 14:52:45 +0200

    Create isPermutation method

--- Arrays and Strings/ArraysAndStrings/PermutationIdentifier.cs
+++ Arrays and Strings/ArraysAndStrings/PermutationIdentifier.cs
@@ -1,6 +1,12 @@
-﻿namespace ArraysAndStrings
+﻿using System;
+
+namespace ArraysAndStrings
 {
     public class PermutationIdentifier
     {
+        public bool isPermutation(string s1, string s2)
+        {
+            return false;
+        }
     }
 }
