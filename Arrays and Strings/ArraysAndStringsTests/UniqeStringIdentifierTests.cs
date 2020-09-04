4ec7920ce19877bdba3312b839ae2366dd52f64a
Máté Iszlai <iszimate@gmail.com>
2020-09-04 14:10:15 +0200

    Add test to IsUnique return type

--- Arrays and Strings/ArraysAndStringsTests/UniqeStringIdentifierTests.cs
+++ Arrays and Strings/ArraysAndStringsTests/UniqeStringIdentifierTests.cs
@@ -1,10 +1,18 @@
-﻿using NUnit.Framework;
+﻿using ArraysAndStrings;
+using NUnit.Framework;
 
 namespace ArraysAndStringsTests
 {
     [TestFixture]
     public class UniqeStringIdentifierTests
     {
+        private readonly UniqeStringIdentifier _stringIdentifier = new UniqeStringIdentifier();
 
+        [Test]
+        public void IsUniqueReturnBool()
+        {
+            var actual = _stringIdentifier.IsUnique("abc");
+            Assert.That(actual == true || actual == false);
+        }
     }
 }
