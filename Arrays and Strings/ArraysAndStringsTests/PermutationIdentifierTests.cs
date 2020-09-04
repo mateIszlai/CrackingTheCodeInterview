8b0009d9544dd1f9c902ab655989124d2f7a64b8
Máté Iszlai <iszimate@gmail.com>
2020-09-04 14:47:26 +0200

    Create classes to Check Permutation excercise

---
+++ Arrays and Strings/ArraysAndStringsTests/PermutationIdentifierTests.cs
@@ -0,0 +1,11 @@
+﻿using ArraysAndStrings;
+using NUnit.Framework;
+
+namespace ArraysAndStringsTests
+{
+    [TestFixture]
+    public class PermutationIdentifierTests
+    {
+        private readonly PermutationIdentifier _permutationIdentifier = new PermutationIdentifier();
+    }
+}
