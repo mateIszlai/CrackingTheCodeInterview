4ab905d17d57961e9164928c6ab1544fc4f1af49
Máté Iszlai <iszimate@gmail.com>
2020-09-04 17:00:08 +0200

    Add base test

--- Arrays and Strings/ArraysAndStringsTests/UrlifierTests.cs
+++ Arrays and Strings/ArraysAndStringsTests/UrlifierTests.cs
@@ -7,5 +7,15 @@ namespace ArraysAndStringsTests
     public class UrlifierTests
     {
         private readonly Urlifier _urlifier = new Urlifier();
+
+        [Test]
+        public void UrlifyBaseCase()
+        {
+            var expected = "a%20b";
+            var actual = _urlifier.Urlify(new char[] {'a', ' ', 'b', ' ', ' '}, 3);
+            Assert.AreEqual(expected, actual);
+        }
+
+
     }
 }
