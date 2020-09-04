315cfc432b9b26ca4e8b34af8207883038a9295a
Máté Iszlai <iszimate@gmail.com>
2020-09-04 15:12:20 +0200

    Add long test cases

--- Arrays and Strings/ArraysAndStringsTests/PermutationIdentifierTests.cs
+++ Arrays and Strings/ArraysAndStringsTests/PermutationIdentifierTests.cs
@@ -26,5 +26,35 @@ namespace ArraysAndStringsTests
         {
             Assert.IsFalse(_permutationIdentifier.isPermutation("a", "b"));
         }
+
+        [Test]
+        public void isPermutationReturnFalseWhenTheGivenStringsLengthNotEqual()
+        {
+            Assert.IsFalse(_permutationIdentifier.isPermutation("a", "ab"));
+        }
+
+        [Test]
+        public void isPermutationLong()
+        {
+            Assert.IsTrue(_permutationIdentifier.isPermutation("asdfghjkl", "dafskjghl"));
+        }
+
+        [Test]
+        public void isPermutationLong1()
+        {
+            Assert.IsTrue(_permutationIdentifier.isPermutation("qertwzui", "iteruzqw"));
+        }
+
+        [Test]
+        public void isPermutationLong2()
+        {
+            Assert.IsFalse(_permutationIdentifier.isPermutation("qertwzui", "iteruasd"));
+        }
+
+        [Test]
+        public void isPermutationLong3()
+        {
+            Assert.IsFalse(_permutationIdentifier.isPermutation("asdfgvjkl", "dafskjghl"));
+        }
     }
 }
