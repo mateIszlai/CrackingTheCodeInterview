3ead2d82a71afbd176f287e673c73230b3161b16
Máté Iszlai <iszimate@gmail.com>
2020-09-04 14:57:47 +0200

    Add solution to basic tests

--- Arrays and Strings/ArraysAndStrings/PermutationIdentifier.cs
+++ Arrays and Strings/ArraysAndStrings/PermutationIdentifier.cs
@@ -6,6 +6,9 @@ namespace ArraysAndStrings
     {
         public bool isPermutation(string s1, string s2)
         {
+            if (s1.Length == 1 && s2.Length == 1)
+                return s1 == s2;
+
             return false;
         }
     }
