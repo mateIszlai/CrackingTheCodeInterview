ea58c7348bc54e7abc4f96c1a2f4ec62e122dc63
Máté Iszlai <iszimate@gmail.com>
2020-09-04 14:57:29 +0200

    Add basic tests

--- Arrays and Strings/ArraysAndStringsTests/PermutationIdentifierTests.cs
+++ Arrays and Strings/ArraysAndStringsTests/PermutationIdentifierTests.cs
@@ -14,5 +14,17 @@ namespace ArraysAndStringsTests
             var actual = _permutationIdentifier.isPermutation("abcd", "fjei");
             Assert.That(actual == true || actual == false);
         }
+
+        [Test]
+        public void isPermutationBasic()
+        {
+            Assert.IsTrue(_permutationIdentifier.isPermutation("a", "a"));
+        }
+
+        [Test]
+        public void isPermutationBasic1()
+        {
+            Assert.IsFalse(_permutationIdentifier.isPermutation("a", "b"));
+        }
     }
 }
