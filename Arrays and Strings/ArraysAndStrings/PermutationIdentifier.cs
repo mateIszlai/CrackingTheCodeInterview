24e07ea075d2051b7848c6982a814555f5dc5f56
Máté Iszlai <iszimate@gmail.com>
2020-09-04 15:12:45 +0200

    Implement algorithm

--- Arrays and Strings/ArraysAndStrings/PermutationIdentifier.cs
+++ Arrays and Strings/ArraysAndStrings/PermutationIdentifier.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 
 namespace ArraysAndStrings
 {
@@ -6,10 +6,13 @@ namespace ArraysAndStrings
     {
         public bool isPermutation(string s1, string s2)
         {
+            if (s1.Length != s2.Length)
+                return false;
+
             if (s1.Length == 1 && s2.Length == 1)
                 return s1 == s2;
 
-            return false;
+            return s1.OrderBy(c => c).SequenceEqual(s2.OrderBy(c => c));
         }
     }
 }
