4524fb5ec98c29854b65816e9778a26c4000410c
Máté Iszlai <iszimate@gmail.com>
2020-09-14 14:33:22 +0200

    Implement insert return and no change return

--- Arrays and Strings/ArraysAndStrings/EditIdentifier.cs
+++ Arrays and Strings/ArraysAndStrings/EditIdentifier.cs
@@ -4,6 +4,33 @@
     {
         public bool IsEditedMaxOnce(string before, string after)
         {
+            if (before == after)
+                return true;
+            if(before.Length != after.Length)
+            {
+                var counter = 0;
+                var idx = 0;
+                var shorter = before.Length < after.Length ? before : after;
+                var longer = shorter == before ? after : before;
+                if (longer.Length - shorter.Length > 1)
+                    return false;
+
+                foreach (var c in shorter)
+                {
+                    if(longer[idx] != c)
+                    {
+                        counter++;
+                        if(longer[idx + 1] != c)
+                        {
+                            return false;
+                        }
+                        idx++;
+                    }
+
+                    idx++;
+                }
+                return counter <= 1;
+            }
             return false;
         }
     }
