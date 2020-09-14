16f0fb65661b273a9376db8eb86c563c087ed507
Máté Iszlai <iszimate@gmail.com>
2020-09-14 17:39:09 +0200

    Implement base of the algorithm

--- Arrays and Strings/ArraysAndStrings/StringCompressor.cs
+++ Arrays and Strings/ArraysAndStrings/StringCompressor.cs
@@ -1,10 +1,32 @@
-﻿namespace ArraysAndStrings
+﻿using System.Text;
+
+namespace ArraysAndStrings
 {
     public class StringCompressor
     {
         public string Compress(string s)
         {
-            return string.Empty;
+            var sb = new StringBuilder();
+            var before = '0';
+            var beforeCount = 0;
+            foreach (var c in s)
+            {
+                if (c == before)
+                    beforeCount++;
+                else
+                {
+                    if(before != '0')
+                    {
+                        sb.Append(before);
+                        sb.Append(beforeCount);
+                    }
+                    before = c;
+                    beforeCount = 1;
+                }
+            }
+            sb.Append(before);
+            sb.Append(beforeCount);
+            return sb.ToString();
         }
     }
 }
