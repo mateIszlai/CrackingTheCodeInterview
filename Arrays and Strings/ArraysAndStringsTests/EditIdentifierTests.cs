262e350bf773b474a5111a6f4bc9b09fa1c8cd6d
Máté Iszlai <iszimate@gmail.com>
2020-09-14 14:32:52 +0200

    Add no operaton and insert tests

--- Arrays and Strings/ArraysAndStringsTests/EditIdentifierTests.cs
+++ Arrays and Strings/ArraysAndStringsTests/EditIdentifierTests.cs
@@ -13,5 +13,29 @@ namespace ArraysAndStringsTests
         {
             Assert.That(_editIdentifier.IsEditedMaxOnce("p", "p") == true || _editIdentifier.IsEditedMaxOnce("p", "p") == false);
         }
+
+        [Test]
+        public void NoOperationReturnTrue()
+        {
+            Assert.IsTrue(_editIdentifier.IsEditedMaxOnce("a", "a"));
+        }
+
+        [Test]
+        public void NoOperationReturnTrue1()
+        {
+            Assert.IsTrue(_editIdentifier.IsEditedMaxOnce("abcdwfeugregwga", "abcdwfeugregwga"));
+        }
+
+        [Test]
+        public void InsertReturnTrue()
+        {
+            Assert.IsTrue(_editIdentifier.IsEditedMaxOnce("a", "ab"));
+        }
+
+        [Test]
+        public void InsertReturnTrue1()
+        {
+            Assert.IsTrue(_editIdentifier.IsEditedMaxOnce("avrieubvvba", "avriexubvvba"));
+        }
     }
 }
