4ef4ea8e4d8ed65435c57d9e44a5fa27e8576ce1
Máté Iszlai <iszimate@gmail.com>
2020-09-14 17:56:34 +0200

    Add classes for Zero Matrix task

---
+++ Arrays and Strings/ArraysAndStringsTests/MatrixZeroerTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using ArraysAndStrings;
+
+namespace ArraysAndStringsTests
+{
+    [TestFixture]
+    public class MatrixZeroerTests
+    {
+        private readonly MatrixZeroer _matrixZeroer = new MatrixZeroer();
+    }
+}
