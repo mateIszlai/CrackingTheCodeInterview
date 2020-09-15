eaf3bcce625a2ce0a86d69e9964b9245e6776b7a
Máté Iszlai <iszimate@gmail.com>
2020-09-15 18:25:34 +0200

    Add tests for more number to remove

--- Linked Lists/LinkedListsTests/DuplicateRemoverTests.cs
+++ Linked Lists/LinkedListsTests/DuplicateRemoverTests.cs
@@ -1,5 +1,7 @@
 ﻿using LinkedLists;
 using LinkedLists.Models;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace LinkedListsTests
@@ -8,7 +10,7 @@ namespace LinkedListsTests
     {
         private readonly DuplicateRemover _duplicateRemover = new DuplicateRemover();
         private readonly Node<int> _linkedList = new Node<int>(0);
-        private readonly Node<int> _expected = new Node<int>(0);
+        private Node<int> _expected = new Node<int>(0);
 
 
         public DuplicateRemoverTests()
@@ -30,6 +32,8 @@ namespace LinkedListsTests
         public void RemoveWorksForOneRemove(int toAppend)
         {
             _linkedList.Append(toAppend);
+            _expected = _expected.Delete(toAppend);
+            _expected.Append(toAppend);
             Assert.Equal(_expected, _duplicateRemover.Remove(_linkedList));
         }
 
@@ -46,7 +50,29 @@ namespace LinkedListsTests
             Assert.Equal(_expected, _duplicateRemover.Remove(_linkedList));
         }
 
+        [Theory]
+        [MemberData(nameof(TestInputs))]
+        public void RemoveWorksWhenMoreToRemove(params int[] toAppend)
+        {
+            foreach (var number in toAppend)
+            {
+                _linkedList.Append(number);
+                _expected = _expected.Delete(number);
+                _expected.Append(number);
+            }
+            var actual = _duplicateRemover.Remove(_linkedList);
+            Assert.Equal(_expected, actual);
+        }
 
+        public static IEnumerable<object[]> TestInputs()
+        {
+            yield return new object[] { new int[] {2, 3}};
+            yield return new object[] { new int[] {2, 4, 6}};
+            yield return new object[] { new int[] {1, 5, 7, 8}};
+            yield return new object[] { new int[] {0, 3}};
+            yield return new object[] { new int[] {2, 9}};
+            yield return new object[] { new int[] {0, 9}};
+        }
 
         private void SeedData()
         {
