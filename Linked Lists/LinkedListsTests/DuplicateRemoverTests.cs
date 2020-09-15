29c6cd8bfd0a771bb5454d671687f606b6e8888d
Máté Iszlai <iszimate@gmail.com>
2020-09-15 17:48:37 +0200

    Add tests for remove one duplicate works

--- Linked Lists/LinkedListsTests/DuplicateRemoverTests.cs
+++ Linked Lists/LinkedListsTests/DuplicateRemoverTests.cs
@@ -8,11 +8,48 @@ namespace LinkedListsTests
     {
         private readonly DuplicateRemover _duplicateRemover = new DuplicateRemover();
         private readonly Node<int> _linkedList = new Node<int>(0);
+        private readonly Node<int> _expected = new Node<int>(0);
+
+
+        public DuplicateRemoverTests()
+        {
+            SeedData();
+        }
 
         [Fact]
         public void RemoveReturnCorrectType()
         {
             Assert.IsType<Node<int>>(_duplicateRemover.Remove(_linkedList));
+        }
+
+        [Fact]
+        public void RemoveWorksForOneRemove()
+        {
+            _linkedList.Append(5);
+            Assert.Equal(_expected, _duplicateRemover.Remove(_linkedList));
+        }
+
+        [Fact]
+        public void RemoveWorksForOneRemoveFromHead()
+        {
+            _linkedList.Append(0);
+            Assert.Equal(_expected, _duplicateRemover.Remove(_linkedList));
+        }
+
+        [Fact]
+        public void RemoveWorksForOneRemoveFromEnd()
+        {
+            _linkedList.Append(9);
+            Assert.Equal(_expected, _duplicateRemover.Remove(_linkedList));
+        }
+
+        private void SeedData()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                _linkedList.Append(i);
+                _expected.Append(i);
+            }
         }
     }
 }
